7929f625e1031fa33d8a047fbc99f6e348ffc6c3
AlonRTPve <94314848+AlonRTPve@users.noreply.github.com>
2023-07-27 21:30:52 +0300

    Version 2 (WORKING) missing a few features.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { useState } from "react";
 
-//TOFIX : FIX THE calculateTie function, when reseting the board, next player doesnt reset
-//add yellow background to a button once clicked
 
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick, isColorActive }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button className={"square " + (isColorActive ? "button-yellow" : "")}  onClick={onSquareClick}>
       {value}
     </button>
   );
 }
-export const Scoreboard = () => {
+
+export const Scoreboard = ({ score }) => {
   return (
     <div className="scoreboard">
-      Player1: {score[0]} Player2: {score[1]}
+      <div className="scoreboard-player1">
+        Player1: {score[0]}
+      </div>
+      <div className="scoreboard-player2">
+        Player2: {score[1]}
+      </div>
     </div>
   );
 };
@@ -23,16 +27,17 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [currentText, setText] = useState("Next player: X");
   const [score, setScore] = useState([0, 0]);
-  let status;
+  const [color, setColor] = useState(Array(9).fill(false))
+  const [opponent, setOpponent] = useState()
 
   function handleClick(i) {
     const nextSquares = squares.slice();
+
     if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
     nextSquares[i] = xIsNext ? "X" : "O";
-
     setSquares(nextSquares);
 
     if (calculateTie(nextSquares)) {
@@ -40,23 +45,35 @@ export default function Board() {
     }
 
     if (calculateWinner(nextSquares)) {
-      const winner = calculateWinner(nextSquares);
+      let [winner, winningmoves] = calculateWinner(nextSquares);
+      change_square_colors(winningmoves); // changes the color of the winning squares
       if (winner) {
-        status = "Winner: " + winner;
-        setText(status);
-        winner === "X" ? setScore[0]++ : setScore[1]++;
+        setText("Winner: " + winner)
+        if (winner === "X") {
+          setScore((scores) => [scores[0]++, scores[1]]);
+        } else {
+          setScore((scores) => [scores[0], scores[1]++]);
+        }
 
         return;
       }
     }
-    status = "Next player: " + (xIsNext ? "O" : "X"); //Rerenders the status div with the current text
-    setText(status);
+    setText("Next player: " + (xIsNext ? "O" : "X")); //Rerenders the status div with the current text
     setXIsNext(!xIsNext);
   }
 
   function resetBoard() {
     setSquares(Array(9).fill(null));
+    setColor(Array(9).fill(null));
     setXIsNext(xIsNext);
+  }
+
+  function change_square_colors(winningmoves) {
+    const nextColor = color.slice();
+    for (let i = 0; i < winningmoves.length; i++) {
+      nextColor[winningmoves[i]] = true;
+    }
+    setColor(nextColor);
   }
 
   function calculateTie(nextSquares) {
@@ -65,27 +82,27 @@ export default function Board() {
       return true;
     }
   }
-
-  console.log(score);
   return (
     <>
-      <Scoreboard score={Scoreboard(score)} />
       <div className="place-center">
+        <Scoreboard score={score} />
         <div className="status">{currentText}</div>
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        <div className="Squares">
+          <div className="board-row">
+            <Square value={squares[0]} isColorActive={color[0]}  onSquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} isColorActive={color[1]}  onSquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} isColorActive={color[2]}  onSquareClick={() => handleClick(2)} />
+          </div>
+          <div className="board-row">
+            <Square value={squares[3]} isColorActive={color[3]}  onSquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} isColorActive={color[4]}  onSquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} isColorActive={color[5]}  onSquareClick={() => handleClick(5)} />
+          </div>
+          <div className="board-row">
+            <Square value={squares[6]} isColorActive={color[6]}  onSquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} isColorActive={color[7]}  onSquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} isColorActive={color[8]}  onSquareClick={() => handleClick(8)} />
+          </div>
         </div>
         <button type="button" onClick={resetBoard} className="reset-button">
           Reset Board
@@ -109,7 +126,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return [squares[a], [a,b,c]];
     }
   }
   return null;
